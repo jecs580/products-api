d56951e64dd99a02a3a6a7a73d88c1ffd4fabf2d
Jorge Callisaya <sanchezenrique580@gmail.com>
2020-03-30 20:04:11 -0400

    Conexion con MongoDB y creacion del endpoint para crear un producto

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Post, Put, Delete, Res, HttpStatus } from '@nestjs/common';
-
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body } from '@nestjs/common';
+import { CreateProductDTO } from './dto/product.dto';
+import { ProductService } from './product.service';
 @Controller('product')
 export class ProductController {
-
+    constructor(private productService: ProductService) { }
     @Post('/create')
-    createPost(@Res() res){
-       return res.status(HttpStatus.OK).json({
-           message:'recibido'
+    async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+        const product= await this.productService.createProduct(createProductDTO);
+        return res.status(HttpStatus.OK).json({
+            message: 'recibido',
+           data: product
        });
     }
 }
